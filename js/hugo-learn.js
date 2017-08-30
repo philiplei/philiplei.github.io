b9530e985276ee99c45dee4391ec65c797fecc3d
philiplei <philip.lei@gmail.com>
2017-08-30 17:23:36 +0800

    routine update

--- js/hugo-learn.js
+++ js/hugo-learn.js
@@ -21,9 +21,7 @@ var images = $("div#body-inner img").not(".inline");
 // Wrap image inside a featherlight (to get a full size view in a popup)
 images.wrap(function(){
   var image =$(this);
-  if (!image.parent("a").length) {
-    return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
-  }
+  return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
 });
 
 // Change styles, depending on parameters set to the image
@@ -56,6 +54,13 @@ images.each(function(index){
     }
   }
 });
+
+// Stick the top to the top of the screen when  scrolling
+$("#top-bar").stick_in_parent( {
+ parent: ".sticky-parent",
+ spacer: ".sticky-spacer",
+});
+
 
 jQuery(document).ready(function() {
   // Add link button for every
